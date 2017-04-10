af25ab6792deddb55b4d610fbaecac405298d9e6
Alexey Dobrynin <ag.dobrynin@gmail.com>
2017-04-10 13:30:00 +0400

    Task 1 complite

--- app/Controllers/ControllerTask1.php
+++ app/Controllers/ControllerTask1.php
@@ -16,12 +16,23 @@ class ControllerTask1 extends Controller{
         $title="Задание #1";
         $subject = self::Request("content");
         $matches = [];
+        $result1 = [];
+        $result2 = [];
         //[НАИМЕНОВАНИЕ_ТЕГА:описание]данные[/НАИМЕНОВАНИЕ_ТЕГА]
-        $pattern = "~\[([\w]*)\b[^\]]*>~siU";
+        $pattern = "#\[([\w]+):?([\w\s]*)?\](.+?)(\[/([\w]+)\]|\[)#ius";
         preg_match_all($pattern, $subject , $matches);
-        $result1=$matches;
-        $result2=$subject;
-
-        return self::View('task1.php', compact(["title", "result1", "result2"]));
+        if( count($matches[5]) ){
+            foreach ($matches[5] as $key => $value) {
+                if( $value == $matches[1][$key] ){
+                    //Данные между тегами
+                    $result1[$value] = $matches[3][$key];
+                    //описание тега
+                    if( $matches[2][$key] ){
+                        $result2[$value] = $matches[2][$key];
+                    }
+                }
+            }
+        }
+        return self::View('task1.php', compact(["title", "result1", "result2", "subject"]));
     }
 }
--- views/task1_result.php
+++ views/task1_result.php
@@ -1,10 +1,16 @@
-<div class="row">
-    <div class="col-md-6">
-        <h4>Первый</h4>
-        <div class="code-style"><?php print_r($result1);?></div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <h4>Первый</h4>
+            <div class="code-style"><?php print_r($result1);?></div>
+        </div>
+        <div class="col-md-6">
+            <h4>Второй</h4>
+            <div class="code-style"><?php print_r($result2);?></div>
+        </div>
     </div>
-    <div class="col-md-6">
-        <h4>Второй</h4>
-        <div class="code-style"><?php print_r($result2);?></div>
+    <div class="row">
+        <div class="col-md-12 text-center"><h3>Исходный текст</h3></div>
+        <div class="col-md-12 code-style"><?php print $subject;?></div>
     </div>
 </div>
